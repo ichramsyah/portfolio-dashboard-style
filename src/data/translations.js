3a075f1011053281e897ee0aca0b03a470ca6dc1
ichramsyah <iamabdulrahman15@gmail.com>
2025-06-24 20:54:32 +0700

    add translation.js for translation

---
+++ src/data/translations.js
@@ -0,0 +1,240 @@
+export const translations = {
+  en: {
+    nav: {
+      home: 'Home',
+      about: 'About',
+      achievements: 'Achievements',
+      projects: 'Projects',
+      contact: 'Contact',
+    },
+    home: {
+      greeting: "Hello, I'm Ichramsyah",
+      religion: 'from Jakarta, Indonesia',
+      description:
+        'A passionate and dedicated software developer with a focus on end-to-end development. Good understanding of web technologies with modern frameworks. Accustomed to working collaboratively in a team and committed to building functional, responsive and visually appealing web applications.',
+      service: 'Service',
+      services_paragraph: 'With creativity and deep analysis, I solve user experience challenges, creating solutions that connect people worldwide with integrity.',
+      services: {
+        fullstack: {
+          title: 'Fullstack Developer',
+          description: 'Build complete web applications from user interfaces to server systems and databases',
+        },
+        backend: {
+          title: 'Backend Developer',
+          description: 'Design and build a strong, secure, and scalable server foundation to support the core functionality of the application',
+        },
+        frontend: {
+          title: 'Frontend Developer',
+          description: 'Create interactive, responsive, and visually appealing user interfaces for an optimal web experience',
+        },
+        uiux: {
+          title: 'UI UX Designer',
+          description: 'Designing intuitive user experiences and captivating visual interfaces, making the app easy to use and engaging.',
+        },
+      },
+    },
+    about: {
+      title: 'About Me',
+      bio: `I am Ichramsyah Abdurrachman, an active student at Paramadina University majoring in Computer Engineering, dedicated to becoming a Fullstack Developer. I specialize in building scalable and user-friendly web applications, leveraging technologies like React, Laravel, Node.js, Express.js, with a focus on advanced skills such as Design Patterns and performance optimization.
+
+My journey began with a passion for graphic design and web development. I have mastered Adobe Illustrator and Figma, enhancing my ability to create intuitive user interfaces.
+
+My internship at PT Hino Motor Sales Indonesia in IT Change Management, alongside my role as a Graphic Designer, provided deep insights into IT processes and the integration of design with technology.
+
+As a Fullstack Developer, I successfully built a social platform website for Indonesian film enthusiasts using Laravel, Blade, Tailwind CSS, and MySQL, which I deployed using cPanel.
+
+To further demonstrate my expertise in real-time communication, I also engineered a full-featured chat application using the MERN (MongoDB, Express.js, React, Node.js) stack, integrating Socket.IO to facilitate instant, bi-directional messaging.
+
+I apply Architecture Patterns like MVC and Repository Pattern to ensure clean and maintainable code, while optimizing performance with efficient MySQL queries.
+
+Additionally, I delve into machine learning with Python, such as developing keyword frequency predictions for technology articles on DOAJ.org.
+
+I am eager to contribute to the technology industry with innovative web solutions. Let’s connect to collaborate or explore opportunities together!`,
+      language: 'Language',
+      education: 'Education',
+      experience: 'Work Experience',
+      interests: 'Personal Interests',
+      educationItems: [
+        {
+          logo: 'https://assets.nsd.co.id/images/kampus/logo/Logo-Paramadina-Universitas-Paramadina-Original-PNG.png',
+          degree: 'Bachelor Degree - Informatic Engineering',
+          school: 'Paramadina University',
+          year: '2022 - 2026',
+          description: 'Focused on software development, algorithms, and information systems.',
+          location: '• Jakarta, Indonesia',
+        },
+        {
+          logo: 'https://ppdb.man1kotabogor.id/logoman.png',
+          degree: 'Science',
+          school: 'MAN 1 Bogor',
+          year: '2019 - 2022',
+          description: 'Focused on scientific studies including math and physics.',
+          location: '• Bogor, Indonesia',
+        },
+      ],
+      experienceItems: [
+        {
+          logo: 'https://upload.wikimedia.org/wikipedia/id/5/5d/Logo_Hino.png',
+          position: 'IT Change Management',
+          company: 'Head Office PT. Hino Motor Sales Indonesia',
+          location: 'Jakarta, Indonesia',
+          year: 'Jul 2023 - Nov 2023',
+          time: '- 4 Month',
+          pos: '- Internship',
+          workfrom: '- Onsite',
+          description:
+            'Collaborated with technical teams to translate complex IT concepts into easily understandable visual content. \nCreated visual documentation and communication materials (such as flyers, animated videos, and presentations) to support IT Change Management initiatives. \nEnsured all communication materials adhered to company branding standards and IT governance. \nUtilized tools such as Canva, Adobe Illustrator, and PowerPoint to produce engaging and informative visuals.',
+          current: false,
+        },
+      ],
+    },
+    achievements: {
+      title: 'Achievements',
+      all: 'All',
+      certifications: 'Certifications',
+      awards: 'Awards',
+    },
+    projects: {
+      title: 'Projects',
+      all: 'All Projects',
+      fullstack: 'Full Stack',
+      frontend: 'Frontend',
+      backend: 'Backend',
+      search: 'Search projects...',
+      liveDemo: 'Live Demo',
+      sourceCode: 'Source Code',
+      viewDetails: 'View Details',
+    },
+    contact: {
+      title: 'Contact Me',
+      subtitle: "Let's work together on your next project",
+      name: 'Your Name',
+      email: 'Your Email',
+      subject: 'Subject',
+      message: 'Your Message',
+      send: 'Send Message',
+      location: 'Location',
+      phone: 'Phone',
+      responseTime: 'Response Time: Within 24 hours',
+    },
+  },
+  id: {
+    nav: {
+      home: 'Beranda',
+      about: 'Tentang',
+      achievements: 'Prestasi',
+      projects: 'Proyek',
+      contact: 'Kontak',
+    },
+    home: {
+      greeting: 'Hai, saya Ichramsyah',
+      religion: 'Berasal dari Jakarta, Indonesia',
+      description:
+        'Seorang pengembang perangkat lunak yang bersemangat dan berdedikasi dengan fokus pada pengembangan end-to-end. Memiliki pemahaman yang baik dalam teknologi web dengan framework modern. Terbiasa bekerja secara kolaboratif dalam tim serta berkomitmen untuk membangun aplikasi web yang fungsional, responsif, dan menarik secara visual.',
+      service: 'Layanan',
+      services_paragraph: 'Dengan kreativitas dan analisis mendalam, saya memecahkan tantangan pengalaman pengguna, menciptakan solusi yang menghubungkan orang di seluruh dunia dengan integritas.',
+      services: {
+        fullstack: {
+          title: 'Fullstack Developer',
+          description: 'Membangun aplikasi web lengkap dari antarmuka pengguna hingga sistem server dan database, memastikan solusi yang terintegrasi penuh.',
+        },
+        backend: {
+          title: 'Backend Developer',
+          description: 'Merancang dan membangun fondasi server yang kuat, aman, dan skalabel untuk mendukung fungsionalitas inti aplikasi',
+        },
+        frontend: {
+          title: 'Frontend Developer',
+          description: 'Menciptakan antarmuka pengguna yang interaktif, responsif, dan menarik secara visual untuk pengalaman web yang optimal.',
+        },
+        uiux: {
+          title: 'UI UX Designer',
+          description: 'Mendesain pengalaman pengguna yang intuitif dan antarmuka visual yang menawan, membuat aplikasi mudah digunakan dan menarik.',
+        },
+      },
+    },
+    about: {
+      title: 'Tentang Saya',
+      bio: `Saya Ichramsyah Abdurrachman, seorang mahasiswa aktif di Universitas Paramadina jurusan Teknik Komputer, berdedikasi untuk menjadi seorang Fullstack Developer. Saya memiliki spesialisasi dalam membangun aplikasi web yang skalabel dan mudah digunakan (user-friendly), dengan memanfaatkan teknologi seperti React, Laravel, Node.js, Express.js, serta berfokus pada keahlian lanjutan seperti Design Patterns dan optimasi kinerja.
+
+Perjalanan saya dimulai dari passion terhadap desain grafis dan pengembangan web. Saya telah menguasai Adobe Illustrator dan Figma, yang meningkatkan kemampuan saya dalam menciptakan antarmuka pengguna yang intuitif.
+
+Pengalaman magang saya di PT Hino Motor Sales Indonesia pada bidang IT Change Management, di samping peran sebagai Graphic Designer, memberikan wawasan mendalam tentang proses IT dan integrasi desain dengan teknologi.
+
+Sebagai Fullstack Developer, saya berhasil membangun website platform sosial untuk penggemar film Indonesia menggunakan Laravel, Blade, Tailwind CSS, dan MySQL, yang saya deploy menggunakan cPanel.
+
+Untuk lebih menunjukkan keahlian saya dalam komunikasi real-time, saya juga merekayasa aplikasi chat dengan fitur lengkap menggunakan tumpukan MERN (MongoDB, Express.js, React, Node.js), mengintegrasikan Socket.IO untuk memfasilitasi pesan instan dua arah.
+
+Saya menerapkan Architecture Patterns seperti MVC dan Repository Pattern untuk memastikan kode yang bersih dan mudah dipelihara, sekaligus mengoptimalkan kinerja dengan kueri MySQL yang efisien.
+
+Selain itu, saya mendalami machine learning dengan Python, seperti mengembangkan prediksi frekuensi kata kunci untuk artikel teknologi di DOAJ.org.
+
+Saya sangat antusias untuk berkontribusi pada industri teknologi dengan solusi web yang inovatif. Mari terhubung untuk berkolaborasi atau menjajaki peluang bersama!`,
+      language: 'Bahasa',
+      education: 'Pendidikan',
+      experience: 'Pengalaman Kerja',
+      interests: 'Minat Pribadi',
+      educationItems: [
+        {
+          logo: 'https://assets.nsd.co.id/images/kampus/logo/Logo-Paramadina-Universitas-Paramadina-Original-PNG.png',
+          degree: 'Gelar Sarjana - Teknik Informatika',
+          school: 'Universitas Paramadina',
+          year: '2022 - 2026',
+          description: 'Fokus pada pengembangan perangkat lunak, algoritma, dan sistem informasi.',
+          location: '• Jakarta, Indonesia',
+        },
+        {
+          logo: 'https://ppdb.man1kotabogor.id/logoman.png',
+          degree: 'Ilmu Pengetahuan Alam',
+          school: 'MAN 1 Bogor',
+          year: '2019 - 2022',
+          description: 'Fokus pada studi ilmiah termasuk matematika dan fisika.',
+          location: '• Bogor, Indonesia',
+        },
+      ],
+      experienceItems: [
+        {
+          logo: 'https://upload.wikimedia.org/wikipedia/id/5/5d/Logo_Hino.png',
+          position: 'IT Change Management',
+          company: 'Head Office PT. Hino Motor Sales Indonesia',
+          location: 'Jakarta, Indonesia',
+          year: 'Jul 2023 - Nov 2023',
+          time: '- 4 Bulan',
+          pos: '- Magang',
+          workfrom: '- WFO',
+          description:
+            'Bekerja sama dengan tim teknis untuk menerjemahkan konsep IT yang kompleks menjadi konten visual yang mudah dipahami.\nMembuat dokumentasi visual dan materi komunikasi (seperti flyer, video animasi, dan presentasi) untuk mendukung inisiatif IT Change Management. \nMemastikan semua materi komunikasi sesuai dengan standar branding perusahaan dan tata kelola IT. \nMenggunakan tools seperti Canva, Adobe Illustrator, dan PowerPoint untuk membuat visual yang menarik dan informatif.',
+          current: false,
+        },
+      ],
+    },
+    achievements: {
+      title: 'Prestasi',
+      all: 'Semua',
+      certifications: 'Sertifikasi',
+      awards: 'Penghargaan',
+    },
+    projects: {
+      title: 'Proyek',
+      all: 'Semua Proyek',
+      fullstack: 'Full Stack',
+      frontend: 'Frontend',
+      backend: 'Backend',
+      search: 'Cari proyek...',
+      liveDemo: 'Demo Langsung',
+      sourceCode: 'Kode Sumber',
+      viewDetails: 'Lihat Detail',
+    },
+    contact: {
+      title: 'Hubungi Saya',
+      subtitle: 'Mari bekerja sama dalam proyek Anda selanjutnya',
+      name: 'Nama Anda',
+      email: 'Email Anda',
+      subject: 'Subjek',
+      message: 'Pesan Anda',
+      send: 'Kirim Pesan',
+      location: 'Lokasi',
+      phone: 'Telepon',
+      responseTime: 'Waktu Respon: Dalam 24 jam',
+    },
+  },
+};
