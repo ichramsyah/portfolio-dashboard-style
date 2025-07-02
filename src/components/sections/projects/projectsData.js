628c6e9204ce8270232cc265e7c9c36500e81963
ichramsyah <iamabdulrahman15@gmail.com>
2025-07-03 03:23:13 +0700

    update project favicon

--- src/components/sections/projects/projectsData.js
+++ src/components/sections/projects/projectsData.js
@@ -5,11 +5,11 @@ export const categories = ['all', 'Fullstack', 'Frontend', 'Backend'];
 
 export const projects = [
   {
-    id: 1,
-    image: 'https://i.ytimg.com/vi/2FnM3nW0jvQ/hq720.jpg',
-    tech: [techIcons.mongodb, techIcons.express, techIcons.react, techIcons.nodejs, techIcons.tailwindcss],
-    githubUrl: 'https://github.com/ichramsyah/velochat-fullstack-webApp',
-    demoUrl: 'https://youtu.be/2FnM3nW0jvQ?feature=shared',
+    id: 4,
+    image: './images/portfolio-dashboard.png',
+    tech: [techIcons.javascript, techIcons.react, techIcons.tailwindcss],
+    githubUrl: 'https://github.com/ichramsyah/portfolio-dashboard-style',
+    demoUrl: 'http://ichram-portfolio-v2.vercel.app/',
   },
   {
     id: 2,
@@ -19,19 +19,21 @@ export const projects = [
     demoUrl: 'https://rinemaa.paramadina.ac.id',
   },
   {
+    id: 1,
+    image: 'https://i.ytimg.com/vi/2FnM3nW0jvQ/hq720.jpg',
+    tech: [techIcons.mongodb, techIcons.express, techIcons.react, techIcons.nodejs, techIcons.tailwindcss],
+    githubUrl: 'https://github.com/ichramsyah/velochat-fullstack-webApp',
+    demoUrl: 'https://youtu.be/2FnM3nW0jvQ?feature=shared',
+  },
+
+  {
     id: 3,
     image: './images/rinema-rebuild.png',
     tech: [techIcons.javascript, techIcons.react, techIcons.tailwindcss],
     githubUrl: 'https://github.com/ichramsyah/rinema-frontend-rebuild-filmPage',
     demoUrl: 'https://rinema-frontend-rebuild-film-page.vercel.app/',
   },
-  {
-    id: 4,
-    image: './images/portfolio-dashboard.JPG',
-    tech: [techIcons.javascript, techIcons.react, techIcons.tailwindcss],
-    githubUrl: 'https://github.com/ichramsyah/portfolio-dashboard-style',
-    demoUrl: 'http://ichram-portfolio-v2.vercel.app/',
-  },
+
   {
     id: 5,
     image: './images/portfolio-dashboard.JPG',
