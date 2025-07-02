74f627dde779b758a31811b6343900ae65e53ce0
ichramsyah <iamabdulrahman15@gmail.com>
2025-07-02 17:04:59 +0700

    update sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -38,7 +38,7 @@ const Sidebar = ({ activeSection, setActiveSection, isMobileMenuOpen, setIsMobil
             <div className="flex items-center justify-between">
               <div className="flex items-center">
                 <img src="./images/ppinstagram.png" alt="" className="w-8 object-cover rounded-full" />
-                <h1 className="text-xl font-medium text-gray-9 dark:text-whitee pl-[6px]">Ichramabdr</h1>
+                <h1 className="text-xl font-semibold text-gray-9 dark:text-whitee pl-[6px]">Ichramabdr</h1>
                 <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" className="text-blue-400 ml-1 mt-[2px]" height="18" width="18" xmlns="http://www.w3.org/2000/svg">
                   <path fill="none" d="M0 0h24v24H0z"></path>
                   <path d="m23 12-2.44-2.79.34-3.69-3.61-.82-1.89-3.2L12 2.96 8.6 1.5 6.71 4.69 3.1 5.5l.34 3.7L1 12l2.44 2.79-.34 3.7 3.61.82L8.6 22.5l3.4-1.47 3.4 1.46 1.89-3.19 3.61-.82-.34-3.69L23 12zm-12.91 4.72-3.8-3.81 1.48-1.48 2.32 2.33 5.85-5.87 1.48 1.48-7.33 7.35z"></path>
