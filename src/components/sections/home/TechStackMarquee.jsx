0e9e0a362d50b11f5ca492b6d93a27a07518ad47
ichramsyah <iamabdulrahman15@gmail.com>
2025-07-16 17:58:55 +0700

    update: add next js and typescript in techstackmarque

--- src/components/sections/home/TechStackMarquee.jsx
+++ src/components/sections/home/TechStackMarquee.jsx
@@ -3,11 +3,13 @@ import { motion } from 'framer-motion';
 const techStack = [
   'React',
   'Node.js',
+  'Next.js',
   'Express.js',
   'PHP',
   'Laravel',
   'Python',
   'Javascript',
+  'Typescript',
   'CSS3',
   'HTML5',
   'Tailwind CSS',
@@ -24,17 +26,18 @@ const techStack = [
   'Framer Motion',
   'React Router',
   'Vite',
-  'Notion',
 ];
 
 const iconMap = {
   React: <img src="https://cdn.simpleicons.org/react" alt="React" className="w-[22px] " />,
   'Node.js': <img src="https://cdn.simpleicons.org/nodedotjs" alt="Node.js" className="w-[22px] " />,
+  'Next.js': <img src="https://www.svgrepo.com/show/354113/nextjs-icon.svg" alt="Next.js" className="w-[22px] " />,
   'Express.js': <img src="https://cdn.simpleicons.org/express" alt="Express.js" className="w-[22px] " />,
   PHP: <img src="https://cdn.simpleicons.org/php" alt="PHP" className="w-[22px] " />,
   Laravel: <img src="https://cdn.simpleicons.org/laravel" alt="Laravel" className="w-[22px] " />,
   Python: <img src="https://www.svgrepo.com/show/452091/python.svg" alt="Python" className="w-[22px] " />,
   Javascript: <img src="https://cdn.simpleicons.org/javascript" alt="JavaScript" className="w-[22px] " />,
+  Typescript: <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/2048px-Typescript_logo_2020.svg.png" alt="Typescript" className="w-[22px] " />,
   CSS3: <img src="https://cdn-icons-png.flaticon.com/512/5968/5968242.png" alt="CSS" className="w-[22px] " />,
   HTML5: <img src="https://cdn.simpleicons.org/html5" alt="HTML" className="w-[22px] " />,
   'Tailwind CSS': <img src="https://cdn.simpleicons.org/tailwindcss" alt="Tailwind CSS" className="w-[22px] " />,
@@ -51,7 +54,6 @@ const iconMap = {
   'Framer Motion': <img src="https://cdn.worldvectorlogo.com/logos/framer-motion.svg" alt="Framer Motion" className="w-[22px] " />,
   'React Router': <img src="https://www.svgrepo.com/show/354262/react-router.svg" alt="React Router" className="w-[22px] " />,
   Vite: <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Vitejs-logo.svg/2078px-Vitejs-logo.svg.png" alt="" className="w-[22px] " />,
-  Notion: <img src="https://upload.wikimedia.org/wikipedia/commons/4/45/Notion_app_logo.png" className="w-[22px] " />,
 };
 
 const TechStackItem = ({ tech }) => (
