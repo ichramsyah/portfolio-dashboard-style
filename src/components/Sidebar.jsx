552ab8dc48cb76a40b35cac08a9e29621014063c
ichramsyah <iamabdulrahman15@gmail.com>
2025-08-12 12:46:57 +0700

    update sidebar: update top sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -37,18 +37,29 @@ const Sidebar = ({ activeSection, setActiveSection, isMobileMenuOpen, setIsMobil
 
       <div className={`fixed left-0 top-0 h-full w-68 bg-whitee px-7 py-7 dark:bg-gray-9 z-50 transform transition-all ${isMobileMenuOpen ? 'translate-x-0' : '-translate-x-full lg:translate-x-0'}`}>
         <div className="flex flex-col h-full">
-          <div className="pb-5 border-b border-gray-2 dark:border-gray-7">
+          <div className="pb-5 border-b border-gray-2 dark:border-gray-7 relative">
             <div className="flex items-center justify-between">
-              <motion.div initial={{ opacity: 0, scale: 0.8 }} whileInView={{ opacity: 1, scale: 1 }} transition={{ duration: 0.8, ease: 'easeOut', delay: 0.1 }} viewport={{ once: false }} className="flex items-center">
-                <img src="./images/ppinstagram.png" alt="" className="w-7 object-cover rounded-full" />
-                <h1 className="text-lg text-gray-9 dark:text-whitee pl-[6px]">Ichramabdr</h1>
-                <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" className="text-blue-4 ml-1" height="18" width="18" xmlns="http://www.w3.org/2000/svg">
-                  <path fill="none" d="M0 0h24v24H0z"></path>
-                  <path d="m23 12-2.44-2.79.34-3.69-3.61-.82-1.89-3.2L12 2.96 8.6 1.5 6.71 4.69 3.1 5.5l.34 3.7L1 12l2.44 2.79-.34 3.7 3.61.82L8.6 22.5l3.4-1.47 3.4 1.46 1.89-3.19 3.61-.82-.34-3.69L23 12zm-12.91 4.72-3.8-3.81 1.48-1.48 2.32 2.33 5.85-5.87 1.48 1.48-7.33 7.35z"></path>
-                </svg>
+              <motion.div
+                initial={{ opacity: 0, scale: 0.8 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 0.8, ease: 'easeOut', delay: 0.1 }}
+                viewport={{ once: false }}
+                className="flex flex-col items-center w-full mt-4" // Menggunakan flex-col dan items-center
+              >
+                {/* Gambar Profil */}
+                <img src="./images/ppinstagram.png" alt="" className="w-20 h-20 object-cover rounded-full border-2 dark:border-gray-700" />
+
+                {/* Grup Nama & Ikon */}
+                <div className="flex items-center gap-1 mt-4">
+                  <h1 className="text-xl text-gray-9 dark:text-whitee">Ichramabdr</h1>
+                  <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" className="text-blue-4" height="20" width="20" xmlns="http://www.w3.org/2000/svg">
+                    <path fill="none" d="M0 0h24v24H0z"></path>
+                    <path d="m23 12-2.44-2.79.34-3.69-3.61-.82-1.89-3.2L12 2.96 8.6 1.5 6.71 4.69 3.1 5.5l.34 3.7L1 12l2.44 2.79-.34 3.7 3.61.82L8.6 22.5l3.4-1.47 3.4 1.46 1.89-3.19 3.61-.82-.34-3.69L23 12zm-12.91 4.72-3.8-3.81 1.48-1.48 2.32 2.33 5.85-5.87 1.48 1.48-7.33 7.35z"></path>
+                  </svg>
+                </div>
               </motion.div>
-              <button onClick={() => setIsMobileMenuOpen(false)} className="lg:hidden p-2 text-gray-5 hover:text-gray-7 dark:text-gray-4 dark:hover:text-gray-2">
-                <X size={20} />
+              <button onClick={() => setIsMobileMenuOpen(false)} className="absolute top-[-10px] right-[-10px] lg:hidden p-2 text-gray-5 hover:text-gray-7 dark:text-gray-4 dark:hover:text-gray-2">
+                <X size={24} />
               </button>
             </div>
           </div>
