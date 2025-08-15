4b099e08f04e6462cdf11a312c7b90d98ecb27e1
ichramsyah <iamabdulrahman15@gmail.com>
2025-08-15 19:39:41 +0700

    add skill in home section

--- src/components/sections/HomeSection.jsx
+++ src/components/sections/HomeSection.jsx
@@ -1,12 +1,15 @@
 import GithubContribution from '../sections/home/GithubContribution';
 import Greeting from '../sections/home/Greeting';
 import Services from '../sections/home/Services';
+import Skills from './home/Skills';
 
 const HomeSection = ({ setActiveSection }) => {
   return (
     <div className="py-12">
       <div className="mx-auto">
         <Greeting />
+        <Skills />
+
         <GithubContribution />
         <Services setActiveSection={setActiveSection} />
       </div>
