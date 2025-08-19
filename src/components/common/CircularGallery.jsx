cd85acf18996a589f21ad12c5648c8901a09e412
ichramsyah <iamabdulrahman15@gmail.com>
2025-08-19 16:00:41 +0700

    update circullar galery

--- src/components/common/CircularGallery.jsx
+++ src/components/common/CircularGallery.jsx
@@ -307,7 +307,6 @@ class App {
   createMedias(items, bend = 1, textColor, borderRadius, font) {
     const defaultItems = [
       { image: `./images/iam.jpeg`, text: '' },
-      { image: `./images/iam2.jpg`, text: '' },
       { image: `./images/iam3.jpeg`, text: '' },
       { image: `./images/iam4.jpeg`, text: '' },
     ];
