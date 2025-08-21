9a0bc7b5c984115fbb9c8eb186eba8c6d7660f07
ichramsyah <iamabdulrahman15@gmail.com>
2025-08-21 17:53:50 +0700

    update verified in sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -54,7 +54,9 @@ const Sidebar = ({ activeSection, setActiveSection, isMobileMenuOpen, setIsMobil
                       <path fill="none" d="M0 0h24v24H0z"></path>
                       <path d="m23 12-2.44-2.79.34-3.69-3.61-.82-1.89-3.2L12 2.96 8.6 1.5 6.71 4.69 3.1 5.5l.34 3.7L1 12l2.44 2.79-.34 3.7 3.61.82L8.6 22.5l3.4-1.47 3.4 1.46 1.89-3.19 3.61-.82-.34-3.69L23 12zm-12.91 4.72-3.8-3.81 1.48-1.48 2.32 2.33 5.85-5.87 1.48 1.48-7.33 7.35z"></path>
                     </svg>
-                    <span className="absolute left-1/2 -translate-x-1/2 -top-7 scale-0 group-hover:scale-100 transition-transform bg-gray-7 dark:bg-gray-3 dark:text-gray-8 text-whitee text-xs px-2 py-1 rounded-md shadow-md">Verified</span>
+                    <span className="absolute left-7 -translate-x-1/2 -top-8 scale-0 group-hover:scale-100 transition-transform duration-200 bg-gray-7 dark:bg-gray-3 dark:text-gray-8 text-whitee text-xs px-2 py-1 rounded-sm shadow-md">
+                      Verified
+                    </span>
                   </div>
                 </div>
 
