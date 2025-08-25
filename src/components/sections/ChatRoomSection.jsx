db4123dc7c310bb39dc5680c00091bc6c53e5611
ichramsyah <iamabdulrahman15@gmail.com>
2025-08-25 20:55:58 +0700

    fix bug, feat:add chatroom

--- src/components/sections/ChatRoomSection.jsx
+++ src/components/sections/ChatRoomSection.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import SectionHeader from '../common/SectionHeader';
-import { useContext } from 'react';
-import { LanguageContext } from '../../contexts/LanguageContext';
+// src/components/ChatRoomSection.jsx
 
+import React, { useContext } from 'react';
+import { LanguageContext } from '../../contexts/LanguageContext';
+import SectionHeader from '../common/SectionHeader';
+import ChatInterface from './chatroom/ChatInterface';
 const ChatRoomSection = () => {
   const { t } = useContext(LanguageContext);
+
   return (
     <div className="py-12 md:mt-0 mt-3">
-      {/* <SectionHeader title={t('achievements.title')} description={t('achievements.description')} /> */}
       <SectionHeader title={t('chatroom.title')} description={t('chatroom.description')} />
+
+      {/* Cukup letakkan komponen chat di sini */}
+      <div className="mt-8">
+        <ChatInterface />
+      </div>
     </div>
   );
 };
---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,30 @@
+// src/contexts/AuthContext.js
+import { createContext, useState, useEffect, useContext } from 'react';
+import { auth } from '../firebase';
+import { onAuthStateChanged } from 'firebase/auth';
+
+export const AuthContext = createContext();
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+  };
+
+  return <AuthContext.Provider value={value}>{!loading && children}</AuthContext.Provider>;
+};
