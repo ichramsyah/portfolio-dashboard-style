ba2d7474fc94761ff58580c5c6a994f9b425b696
ichramsyah <iamabdulrahman15@gmail.com>
2025-09-17 14:48:32 +0700

    update skills

--- src/components/sections/home/Services.jsx
+++ src/components/sections/home/Services.jsx
@@ -24,7 +24,7 @@ const Services = ({ setActiveSection }) => {
 
   return (
     <div className="mt-8">
-      <div className="flex items-center text-xl text-gray-9 dark:text-whitee mb-4">
+      <div className="flex items-center text-xl text-gray-9 dark:text-gray-3 mb-4">
         <svg stroke="currentColor" fill="currentColor" stroke-width="0" role="img" viewBox="0 0 24 24" height="22" width="22" xmlns="http://www.w3.org/2000/svg">
           <path d="M0 10.435c0-.256.051-.512.153-.758.233-.56.854-1.046 2.095-2.018l6.206-4.856c1.241-.972 1.862-1.458 2.577-1.64.63-.16 1.308-.16 1.938 0 .715.183 1.336.668 2.577 1.64l6.206 4.856c1.241.972 1.862 1.458 2.095 2.018.102.246.153.502.153.758v3.13c0 .256-.051.512-.153.758-.233.56-.854 1.046-2.095 2.017l-6.206 4.857c-1.241.972-1.862 1.457-2.577 1.64-.63.16-1.308.16-1.938 0-.715-.183-1.336-.668-2.577-1.64L2.248 16.34C1.007 15.37.386 14.883.153 14.323A1.971 1.971 0 0 1 0 13.565v-3.13Zm9.34-3.238.887.694c.62.485.93.728 1.289.82.315.08.653.08.968 0 .358-.092.668-.335 1.29-.82l.886-.694c.62-.486.93-.729 1.047-1.009a.975.975 0 0 0 0-.758c-.116-.28-.427-.523-1.047-1.008l-.887-.694c-.62-.486-.93-.729-1.289-.82a1.984 1.984 0 0 0-.968 0c-.358.091-.668.334-1.29.82l-.886.694c-.62.485-.93.728-1.047 1.008a.975.975 0 0 0 0 .758c.116.28.427.523 1.047 1.009Zm5.91 4.625.887.694c.62.486.931.729 1.29.82.314.08.653.08.968 0 .358-.091.668-.334 1.288-.82l.887-.694c.62-.485.931-.728 1.047-1.008a.976.976 0 0 0 0-.758c-.116-.28-.426-.523-1.047-1.009l-.887-.694c-.62-.485-.93-.728-1.288-.82a1.984 1.984 0 0 0-.969 0c-.358.092-.668.335-1.289.82l-.886.694c-.621.486-.931.729-1.047 1.009a.975.975 0 0 0 0 .758c.116.28.426.523 1.047 1.008Zm-11.82 0 6.797 5.32c.62.486.93.728 1.289.82.315.08.653.08.968 0 .358-.092.668-.334 1.29-.82l.886-.694c.62-.486.93-.729 1.047-1.009a.974.974 0 0 0 0-.758c-.116-.28-.427-.523-1.047-1.008l-6.797-5.32c-.62-.485-.931-.728-1.29-.82a1.984 1.984 0 0 0-.968 0c-.358.092-.668.335-1.288.82l-.887.694c-.62.486-.931.729-1.047 1.009a.975.975 0 0 0 0 .758c.116.28.426.523 1.047 1.008Z"></path>
         </svg>
@@ -34,7 +34,7 @@ const Services = ({ setActiveSection }) => {
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 md:gap-[10px] gap-3 mt-6">
         {/* 1 */}
-        <SpotlightCard className="h-76 rounded-lg">
+        <SpotlightCard className="h-76 rounded-lg bg-gray-1/40 dark:bg-gray-9/10">
           <div className="flex flex-col items-center justify-center p-5">
             <LayoutGrid width={40} height={40} className="text-gray-7 dark:text-gray-2 p-2 rounded-md" />
             <span className="pt-4 text-gray-8 dark:text-gray-2">{t('home.skills')}</span>
@@ -44,7 +44,7 @@ const Services = ({ setActiveSection }) => {
         </SpotlightCard>
 
         {/* 2 */}
-        <SpotlightCard className="h-76 rounded-lg">
+        <SpotlightCard className="h-76 rounded-lg bg-gray-1/40 dark:bg-gray-9/10">
           <div className="flex flex-col items-center justify-center p-5">
             <button onClick={() => setActiveSection('about')}>
               <User width={40} height={40} className="text-gray-7 transition-all cursor-pointer hover:bg-neutral-1 hover:shadow-sm dark:hover:bg-gray-8 hover:scale-105 dark:text-gray-2 p-2 rounded-md" />
@@ -58,7 +58,7 @@ const Services = ({ setActiveSection }) => {
         </SpotlightCard>
 
         {/* 3 */}
-        <SpotlightCard className="h-76 rounded-lg lg:col-span-2 w-full h-full flex">
+        <SpotlightCard className="h-76 rounded-lg bg-gray-1/40 dark:bg-gray-9/10 lg:col-span-2 w-full h-full flex">
           <div className="w-1/2 py-5 px-6">
             <button onClick={() => setActiveSection('projects')}>
               <Layers width={40} height={40} className="text-gray-7 transition-all cursor-pointer hover:bg-neutral-1 hover:shadow-sm dark:hover:bg-gray-8 hover:scale-105 dark:text-gray-2 p-2 rounded-md" />
@@ -90,7 +90,7 @@ const Services = ({ setActiveSection }) => {
         </SpotlightCard>
 
         {/* 4 */}
-        <SpotlightCard className="h-76 rounded-lg lg:col-span-2 w-full h-full flex">
+        <SpotlightCard className="h-76 rounded-lg bg-gray-1/40 dark:bg-gray-9/10 lg:col-span-2 w-full h-full flex">
           <div className="w-1/2 py-5 px-6">
             <button>
               <Brush width={40} height={40} className="text-gray-7 dark:text-gray-2 p-2 rounded-md" />
@@ -107,7 +107,7 @@ const Services = ({ setActiveSection }) => {
         </SpotlightCard>
 
         {/* 5 */}
-        <SpotlightCard className="h-76 rounded-lg">
+        <SpotlightCard className="h-76 rounded-lg bg-gray-1/40 dark:bg-gray-9/10">
           <div className="flex flex-col items-center justify-center p-5">
             <button onClick={() => setActiveSection('chatroom')}>
               <MessageSquareText width={40} height={40} className="text-gray-7 transition-all cursor-pointer hover:bg-neutral-1 hover:shadow-sm dark:hover:bg-gray-8 hover:scale-105 dark:text-gray-2 p-2 rounded-md" />
@@ -131,7 +131,7 @@ const Services = ({ setActiveSection }) => {
         </SpotlightCard>
 
         {/* 6 */}
-        <SpotlightCard className="h-76 rounded-lg">
+        <SpotlightCard className="h-76 rounded-lg bg-gray-1/40 dark:bg-gray-9/10">
           <div className="flex flex-col items-center justify-center p-5">
             <button onClick={() => setActiveSection('achievements')}>
               <Award width={40} height={40} className="text-gray-7 transition-all cursor-pointer hover:bg-neutral-1 hover:shadow-sm dark:hover:bg-gray-8 hover:scale-105 dark:text-gray-2 p-2 rounded-md" />
