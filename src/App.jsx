41852cc8afa503bc0de0401f2bd402ddf48a30e7
ichramsyah <iamabdulrahman15@gmail.com>
2025-09-19 23:54:28 +0700

    update preloader, sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ const App = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 0);
+    }, 2000);
 
     return () => clearTimeout(timer);
   }, []);
