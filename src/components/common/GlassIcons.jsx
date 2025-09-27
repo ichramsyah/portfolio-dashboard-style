14971f8ab5569f0cf367de3f7532b7ae0e511054
ichramsyah <iamabdulrahman15@gmail.com>
2025-09-28 00:50:46 +0700

    update skills & glass icons

--- src/components/common/GlassIcons.jsx
+++ src/components/common/GlassIcons.jsx
@@ -3,6 +3,7 @@ const colorClassMapping = {
   nextjs: 'bg-gradient-to-br from-gray-800 to-gray-900',
   laravel: 'bg-gradient-to-br from-red-600 to-red-700',
   express: 'bg-gradient-to-br from-gray-600 to-gray-700',
+  django: 'bg-gradient-to-br from-green-600 to-green-900',
   javascript: 'bg-gradient-to-br from-yellow-400 to-yellow-500',
   typescript: 'bg-gradient-to-br from-blue-600 to-blue-700',
   php: 'bg-gradient-to-br from-indigo-400 to-purple-500',
@@ -36,10 +37,10 @@ const GlassIcons = ({ items, className }) => {
             key={index}
             type="button"
             aria-label={item.label}
-            className={`relative bg-transparent outline-none w-[3.7em] h-[3.7em] [perspective:24em] [transform-style:preserve-3d] [-webkit-tap-highlight-color:transparent] group ${item.customClass || ''}`}
+            className={`relative bg-transparent outline-none w-[3.8em] h-[3.8em] [perspective:24em] [transform-style:preserve-3d] [-webkit-tap-highlight-color:transparent] group ${item.customClass || ''}`}
           >
             <span
-              className={`absolute top-0 left-0 w-full h-full rounded-[1.25em] block transition-[opacity,transform] duration-300 ease-[cubic-bezier(0.83,0,0.17,1)] origin-[100%_100%] rotate-[15deg] group-hover:[transform:rotate(25deg)_translate3d(-0.5em,-0.5em,0.5em)] ${backgroundClasses}`}
+              className={`absolute top-0 left-0 w-full h-full rounded-[1.25em] block transition-[opacity,transform] duration-300 ease-[cubic-bezier(0.83,0,0.17,1)] origin-[100%_100%] rotate-[18deg] group-hover:[transform:rotate(16deg)_translate3d(-0.4em,-0.3em,0.2em)] ${backgroundClasses}`}
             ></span>
 
             <span
@@ -53,7 +54,7 @@ const GlassIcons = ({ items, className }) => {
               </span>
             </span>
 
-            <span className="absolute top-full left-0 right-0 text-center text-gray-8 dark:text-gray-2 whitespace-nowrap leading-[2] text-base opacity-0 transition-[opacity,transform] duration-300 ease-[cubic-bezier(0.83,0,0.17,1)] translate-y-0 group-hover:opacity-100 group-hover:[transform:translateY(20%)]">
+            <span className="absolute top-full left-0 right-0 text-center text-[15px] text-gray-7 dark:text-gray-2 whitespace-nowrap leading-[2] text-base opacity-0 transition-[opacity,transform] duration-300 ease-[cubic-bezier(0.83,0,0.17,1)] translate-y-0 group-hover:opacity-100 group-hover:[transform:translateY(20%)]">
               {item.label}
             </span>
           </button>
--- src/components/sections/home/Skills.jsx
+++ src/components/sections/home/Skills.jsx
@@ -1,7 +1,7 @@
 import { CodeBracketIcon } from '@heroicons/react/16/solid';
 import GlassIcons from '../../common/GlassIcons';
 import { FaReact, FaLaravel, FaPhp, FaBootstrap, FaCss3Alt, FaJs, FaGitAlt, FaGithub, FaFigma, FaPython, FaNodeJs, FaLinux } from 'react-icons/fa';
-import { SiNextdotjs, SiExpress, SiTypescript, SiTailwindcss, SiMongodb, SiPostman, SiAdobeillustrator, SiCpanel, SiFirebase } from 'react-icons/si';
+import { SiNextdotjs, SiExpress, SiTypescript, SiTailwindcss, SiMongodb, SiPostman, SiAdobeillustrator, SiCpanel, SiFirebase, SiDjango } from 'react-icons/si';
 import { GrMysql } from 'react-icons/gr';
 import { DiHtml5 } from 'react-icons/di';
 import { useContext } from 'react';
@@ -14,6 +14,7 @@ const Skills = () => {
     { icon: <SiNextdotjs size={40} className="text-gray-9 dark:text-gray-1 transition-color duration-500" />, colorKey: 'nextjs', label: 'Next.js' },
     { icon: <FaLaravel size={40} className="text-gray-9 dark:text-gray-1 transition-color duration-500" />, colorKey: 'laravel', label: 'Laravel' },
     { icon: <SiExpress size={40} className="text-gray-9 dark:text-gray-1 transition-color duration-500" />, colorKey: 'express', label: 'Express.js' },
+    { icon: <SiDjango size={26} className="text-gray-9 dark:text-gray-1 transition-color duration-500" />, colorKey: 'django', label: 'Django' },
     { icon: <FaJs size={30} className="text-gray-9 dark:text-gray-1 transition-color duration-500" />, colorKey: 'javascript', label: 'JavaScript' },
     { icon: <SiTypescript size={27} className="text-gray-9 dark:text-gray-1 transition-color duration-500" />, colorKey: 'typescript', label: 'TypeScript' },
     { icon: <FaNodeJs size={40} className="text-gray-9 dark:text-gray-1 transition-color duration-500" />, colorKey: 'nodejs', label: 'Node.js' },
@@ -30,8 +31,8 @@ const Skills = () => {
     { icon: <FaGitAlt size={40} className="text-gray-9 dark:text-gray-1 transition-color duration-500" />, colorKey: 'git', label: 'Git' },
     { icon: <FaGithub size={40} className="text-gray-9 dark:text-gray-1 transition-color duration-500" />, colorKey: 'github', label: 'GitHub' },
     { icon: <SiPostman size={40} className="text-gray-9 dark:text-gray-1 transition-color duration-500" />, colorKey: 'postman', label: 'Postman' },
-    { icon: <FaFigma size={40} className="text-gray-9 dark:text-gray-1 transition-color duration-500" />, colorKey: 'figma', label: 'Figma' },
-    { icon: <SiAdobeillustrator size={40} className="text-gray-9 dark:text-gray-1 transition-color duration-500" />, colorKey: 'adobeillustrator', label: 'Adobe Illustrator' },
+    { icon: <FaFigma size={32} className="text-gray-9 dark:text-gray-1 transition-color duration-500" />, colorKey: 'figma', label: 'Figma' },
+    { icon: <SiAdobeillustrator size={28} className="text-gray-9 dark:text-gray-1 transition-color duration-500" />, colorKey: 'adobeillustrator', label: 'Adobe Illustrator' },
     { icon: <SiCpanel size={40} className="text-gray-9 dark:text-gray-1 transition-color duration-500" />, colorKey: 'cpanel', label: 'cPanel' },
   ];
 
@@ -44,7 +45,7 @@ const Skills = () => {
         </h3>
         <p className="text-gray-5 dark:text-gray-4">{t('home.skills_paragraph')}</p>
       </div>
-      <div className="md:mb-10 mb-0">
+      <div className="md:mb-1 mb-0">
         <GlassIcons items={items} className="custom-class" />
       </div>
     </div>
