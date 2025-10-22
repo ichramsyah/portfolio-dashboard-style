4a049d0fa831770625ca475780a77b93655e6949
ichramsyah <iamabdulrahman15@gmail.com>
2025-10-22 17:14:39 +0700

    update projectdetail modal, update translation.js velochat

--- src/components/sections/projects/ProjectDetailModal.jsx
+++ src/components/sections/projects/ProjectDetailModal.jsx
@@ -30,7 +30,36 @@ const ProjectDetailModal = ({ project, onClose, t }) => {
               <div className="max-w-none">
                 <p className=" text-gray-6 dark:text-gray-4 mb-4">{item.paragraph}</p>
                 <h3 className="font-semibold text-lg text-gray-8 dark:text-gray-1 mb-3">{item.htechstack}</h3>
-                <p className="text-gray-6 dark:text-gray-4 mb-4">{item.techstack}</p>
+                <ul className="text-gray-6 dark:text-gray-4 list-disc pl-5 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-x-6 gap-y-2">
+                  {item.techstack.split(', ').map((tech, techIndex) => (
+                    <li key={techIndex}>{tech}</li>
+                  ))}
+                </ul>
+                <h3 className="font-semibold text-lg text-gray-8 dark:text-gray-1 mt-6 mb-3">{item.hfeat}</h3>
+                <ul className="text-gray-6 dark:text-gray-4 pl-5 space-y-2">
+                  {item.feat.split('.,').map((feature, featureIndex) => (
+                    <li key={featureIndex} className="flex items-start">
+                      <span className="mr-2">✅</span>
+                      <span>{feature.trim()}.</span>
+                    </li>
+                  ))}
+                </ul>
+                {item.linkyt && (
+                  <div className="mt-6">
+                    <h3 className="font-semibold text-lg text-gray-8 dark:text-gray-1 mb-3">Demo Video</h3>
+                    <div className="aspect-w-16 aspect-h-9">
+                      <iframe
+                        src={item.linkyt}
+                        title="YouTube video player"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowFullScreen
+                        autoPlay
+                        className="w-full h-[360px] rounded-lg"
+                      ></iframe>
+                    </div>
+                  </div>
+                )}
               </div>
             ))}
           </div>
