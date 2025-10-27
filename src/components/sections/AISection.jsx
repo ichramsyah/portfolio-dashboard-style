caffe9cb4d3c3d7167925daba979a7bc0eee79e1
ichramsyah <iamabdulrahman15@gmail.com>
2025-10-27 11:56:36 +0700

    feat: add AI Assistent

---
+++ src/components/sections/AISection.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SectionHeader from '../common/SectionHeader';
+
+const AISection = () => {
+  return (
+    <div className="py-14 lg:py-8 md:mt-0 mt-3">
+      <SectionHeader title={'AI Assistant'} description={'Comming Soon'} />
+
+      <div className="mt-8"></div>
+    </div>
+  );
+};
+
+export default AISection;
