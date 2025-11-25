8613ec401b93c76326e0b8dab40c450b0621a80a
ichramsyah <iamabdulrahman15@gmail.com>
2025-11-25 23:14:51 +0700

    setup AI Assistant

--- src/components/sections/AISection.jsx
+++ src/components/sections/AISection.jsx
@@ -6,7 +6,7 @@ const AISection = () => {
     <div className="py-14 lg:py-8 md:mt-0 mt-3">
       <SectionHeader title={'AI Assistant'} description={'Comming Soon'} />
 
-      <div className="mt-8"></div>
+      <div className="mt-8">{/* disini */}</div>
     </div>
   );
 };
